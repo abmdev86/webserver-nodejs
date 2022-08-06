538b650904fbbc2572c7c88f783b9ff424d362e4
Auroiah Morgan <amwebdev@outlook.com>
2022-08-05 23:32:29 -0400

    init commit

---
+++ router/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+
+router.use((req, res, next)=>{
+    console.log('Time: ', Date.now());
+    next();
+})
+
+router.get("/", (req, res) => {
+  res.send("Hello from router.");
+});
+
+module.exports = router;
