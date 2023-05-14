93b6d58c525f2896f26e778f7450df0fdcae0b67
Auroiah Morgan <amwebdev@outlook.com>
2023-05-14 10:20:19 -0400

    Add local email sending

--- router/router.js
+++ router/router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require(`path`);
+const sgMail = require("@sendgrid/mail");
 const router = express.Router();
 
 router.use((req, res, next) => {
   console.log("Time: ", Date.now());
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   next();
 });
 
@@ -16,12 +19,26 @@ router.get("/submit", (req, res) => {
 });
 
 router.post("/submit", (req, res) => {
-  console.log({
-    from: req.body.from,
-    name: req.body.name,
-    message: req.body.message,
-  });
-  res.send("Thank you for your message!");
+  const msg = {
+    to: "me@aurmordesigns.net",
+    from: "me@aurmordesigns.net",
+    subject: req.body.subject,
+    text: `message from: ${req.body.from}, ${req.body.message}`,
+    html: `<div> <p>${req.body.from}</p>, <br/> <p>${req.body.message}</p></div>`,
+  };
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log({
+        response: response,
+      });
+      res.status(response[0].statusCode).send("Thank you for your message!");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(error.code).send(error.ResponseError);
+    });
 });
 
 module.exports = router;
